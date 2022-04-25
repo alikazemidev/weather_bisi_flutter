6e58118f51abfee01c70cbd264f31a3d6983d73d
alikz <ali6316k@gmail.com>
2022-04-25 19:01:05 +0430

    complete static ui

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,7 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(height: 20),
                 Container(
                   width: double.infinity,
-                  height: 100,
+                  height: 120,
                   child: ListView.builder(
                     shrinkWrap: true,
                     itemCount: 10,
@@ -147,15 +147,128 @@ class _HomePageState extends State<HomePage> {
                         padding: const EdgeInsets.all(8.0),
                         child: Column(
                           children: [
-                            Text('Fri 8PM'),
+                            Text(
+                              'Fri 8PM',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            SizedBox(height: 5),
                             Icon(Icons.cloud, color: Colors.white),
+                            SizedBox(height: 5),
                             Text('14 \u00b0')
                           ],
                         ),
                       );
                     },
                   ),
-                )
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 1,
+                  color: Colors.grey,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'wind speed',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '4.73 m/s',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      width: 1,
+                      height: 50,
+                      color: Colors.white,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'sunrise',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '6:19 PM',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      width: 1,
+                      height: 50,
+                      color: Colors.white,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'sunset',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '9:30 AM',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      width: 1,
+                      height: 50,
+                      color: Colors.white,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'humidity',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '72%',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
