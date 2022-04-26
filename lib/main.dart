6372eee9cf5735baa57dff8e0e093717edbb3c3e
ali kazemi <alikazemidev@gmail.comi>
2022-04-26 13:41:50 +0430

    add background and blur and create search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -40,236 +42,290 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController cityNameCotroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      // appBar: AppBar(),
       body: SafeArea(
         child: Container(
-          color: Colors.black,
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'Mountain View',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.white,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('images/1.jpg'),
+            ),
+          ),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 8,
+              sigmaY: 8,
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'Clear Sky',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Icon(
-                  Icons.wb_sunny_outlined,
-                  color: Colors.white,
-                  size: 80,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  '14\u00b0',
-                  style: TextStyle(
-                    fontSize: 60,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
+                  // *search bar
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
                       children: [
-                        Text(
-                          'Max',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          '16\u00b0',
-                          style: Theme.of(context).textTheme.bodyText2,
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 20),
-                      width: 2,
-                      height: 45,
-                      color: Colors.white,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Min',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          '12\u00b0',
-                          style: Theme.of(context).textTheme.bodyText2,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: double.infinity,
-                  height: 1,
-                  color: Colors.grey,
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: double.infinity,
-                  height: 120,
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: 10,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'Fri 8PM',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.grey,
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(149, 163, 163, 163),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: TextField(
+                              controller: cityNameCotroller,
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(10),
+                                border: InputBorder.none,
+                                labelText: 'Enter City Name',
+                                labelStyle: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                            SizedBox(height: 5),
-                            Icon(Icons.cloud, color: Colors.white),
-                            SizedBox(height: 5),
-                            Text('14 \u00b0')
-                          ],
+                          ),
                         ),
-                      );
-                    },
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('find'),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 1,
-                  color: Colors.grey,
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          'wind speed',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          '4.73 m/s',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      width: 1,
-                      height: 50,
+                  SizedBox(
+                    height: 10,
+                  ),
+                  //* top section
+                  Text(
+                    'Mountain View',
+                    style: TextStyle(
+                      fontSize: 35,
                       color: Colors.white,
                     ),
-                    Column(
-                      children: [
-                        Text(
-                          'sunrise',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          '6:19 PM',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      width: 1,
-                      height: 50,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Clear Sky',
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Icon(
+                    Icons.wb_sunny_outlined,
+                    color: Colors.white,
+                    size: 80,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '14\u00b0',
+                    style: TextStyle(
+                      fontSize: 60,
                       color: Colors.white,
                     ),
-                    Column(
-                      children: [
-                        Text(
-                          'sunset',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Max',
+                            style: Theme.of(context).textTheme.bodyText1,
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          '9:30 AM',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
+                          SizedBox(
+                            height: 10,
                           ),
-                        ),
-                      ],
+                          Text(
+                            '16\u00b0',
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        width: 2,
+                        height: 45,
+                        color: Colors.white,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'Min',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            '12\u00b0',
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // *middle section
+                  Container(
+                    width: double.infinity,
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(height: 20),
+                  // *list view weather
+                  Container(
+                    width: double.infinity,
+                    height: 120,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Fri 8PM',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Icon(Icons.cloud, color: Colors.white),
+                              SizedBox(height: 5),
+                              Text('14 \u00b0')
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      width: 1,
-                      height: 50,
-                      color: Colors.white,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'humidity',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(height: 10),
+                  // * extera info weather row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'wind speed',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          '72%',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
+                          SizedBox(height: 10),
+                          Text(
+                            '4.73 m/s',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        width: 1,
+                        height: 50,
+                        color: Colors.white,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'sunrise',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '6:19 PM',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        width: 1,
+                        height: 50,
+                        color: Colors.white,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'sunset',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '9:30 AM',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        width: 1,
+                        height: 50,
+                        color: Colors.white,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'humidity',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '72%',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
